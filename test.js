0e525888aedc1760725b5d4b8ddf23e36b744f0e
Tsurematsu <daniel120802@gmail.com>
2025-10-18 10:32:07 -0500

    up

--- test.js
+++ test.js
@@ -1,19 +1,8 @@
 async () => {
-    // Código para pegar en la consola del navegador (page context)
-    const ws = await fetch('https://tsurematsu.github.io/WelcomePage/ws.js').then(res => res.text())
-    console.log(ws);
-    
-    const swCode = ``;
-
-    // Crear blob y registrar el SW
-    // const blob = new Blob([swCode], { type: 'application/javascript' });
-    // const swUrl = URL.createObjectURL(blob);
-
-    // navigator.serviceWorker.register(swUrl, { scope: '/' })
-    //     .then(reg => {
-    //         console.log('Service Worker registrado (desde blob):', reg);
-    //         console.log('Abre "Application → Service Workers" y pulsa "inspect" para ver la consola del SW.');
-    //     })
-    //     .catch(err => console.error('Error registrando SW:', err));
-
+    const swCode = await fetch('https://tsurematsu.github.io/WelcomePage/ws.js').then(res => res.text());
+    const blob = new Blob([swCode], { type: 'application/javascript' });
+    const swUrl = URL.createObjectURL(blob);
+    const reg = await navigator.serviceWorker.register(swUrl, { scope: '/' })
+    console.log('Service Worker registrado (desde blob):', reg);
+    console.log('Abre "Application → Service Workers" y pulsa "inspect" para ver la consola del SW.');
 }
