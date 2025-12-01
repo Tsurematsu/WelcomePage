049c737fc2f7ee0ac70421c1a88ccaf4dc31d858
Tsurematsu <daniel120802@gmail.com>
2025-12-01 15:35:09 -0500

    update

--- other.js
+++ other.js
@@ -1,2 +1,6 @@
 // fetch('https://tsurematsu.github.io/WelcomePage/test.js').then(res => res.text()).then(r=>eval(`(${r})()`))
 fetch('https://tsurematsu.github.io/WelcomePage/tool_pdf.js').then(res => res.text()).then(r=>eval(`(${r})()`))
+
+const response = await fetch('https://tsurematsu.github.io/WelcomePage/tool_pdf.js');
+    const code = await response.text();
+    eval(code);
